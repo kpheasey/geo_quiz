e03bed673dd42073ccbddf39d21d632f2db34049
Kevin Pheasey <kevin@kpheasey.com>
2016-01-22 09:28:44 -0500

    Rotating CheatActivity does not clear cheating result.

--- app/src/main/java/com/kpheasey/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/kpheasey/android/geoquiz/CheatActivity.java
@@ -13,8 +13,10 @@ public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.kpheasey.android.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN = "com.kpheasey.android.geoquiz.answer_shown";
+    private static final String KEY_ANSWER_SHOWN = "answer_shown";
 
     private boolean mAnswerIsTrue;
+    private boolean mIsAnswerShown;
 
     private TextView mAnswerTextView;
     private Button mShowAnswer;
@@ -47,18 +49,30 @@ public class CheatActivity extends AppCompatActivity {
                 } else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
-                setAnswerShownResult(true);
+                mIsAnswerShown = true;
+                setAnswerShownResult();
             }
         });
+
+        if (savedInstanceState != null) {
+            mIsAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_SHOWN, false);
+        }
+
+        setAnswerShownResult();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
-    private void setAnswerShownResult(boolean isAnswerShown) {
-        Intent data = new Intent();
-        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
-        setResult(RESULT_OK, data);
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_ANSWER_SHOWN, mIsAnswerShown);
     }
 
+    private void setAnswerShownResult() {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, mIsAnswerShown);
+        setResult(RESULT_OK, data);
+    }
 }
